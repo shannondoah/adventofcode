fde818d740cb93b288e34f9454d32d710303f36d
Shannon <shannon@spark.re>
2020-12-12 16:30:44 -0800

    Day 10 but with a result by 2021

--- 2020/day 10/day10.rb
+++ 2020/day 10/day10.rb
@@ -1,43 +1,58 @@
-def get_joltage_differences(adapters)
-  current = 0
+class AdapterSort
+  attr_reader :adapters
 
-  differences = adapters.each_with_object({}) do |adapter, differences|
-    difference = adapter - current
-    differences[difference] = (differences[difference] || 0) + 1
-
-    current = adapter
+  def initialize(adapters)
+    @adapters = adapters << adapters.max + 3 # appends device
   end
 
-  differences[3] += 1 # increment for device
+  def get_joltage_differences
+    current = 0
 
-  differences[1] * differences[3]
+    differences = adapters.each_with_object({}) do |adapter, differences|
+      difference = adapter - current
+      differences[difference] = (differences[difference] || 0) + 1
+
+      current = adapter
+    end
+
+    differences[1] * differences[3]
+  end
+
+  # Starts from the device joltage value and recursively adds
+  # the number of possible paths to each preceding adapter
+  # given a max diff of 3. For each "adapter", the number of
+  # combinations is equal to the sum of the combinations of
+  # the three "adapters" before it. If that "adapter" is not
+  # in your collection, or if the value is lower than 0 (past
+  # the outlet - does not exist), the count for that adapter
+  # will be zero. If the value equals zero, then you have
+  # reached the outlet, which has a single path.
+  #
+  # Caches the results for each possible adapter so when
+  # calculating value[4], we can reuse value[2] and value[1]
+  # from calculating value[3]
+  def count_arrangements(value = adapters.max, cache = {})
+    return cache[value] if cache[value]
+
+    if value.zero?
+      cache[value] = 1
+      return 1
+    elsif value.negative? || !adapters.include?(value)
+      cache[value] = 0
+      return 0
+    end
+
+    cache[value] = (1..3).map do |n|
+      count_arrangements(value-n, cache)
+    end.reduce(:+)
+
+    cache[value]
+  end
 end
 
-def get_distinct_arrangements(adapters)
-  outlet = 0
-  device = adapters.max + 3
-  skippable = []
+adapters = File.read(File.expand_path('input.txt', File.dirname(__FILE__)))
+  .split("\n")
+  .map(&:to_i)
+  .sort!
 
-  adapters.each_with_index do |adapter, i|
-    lookahead = adapters[i+1] || device
-    lookbehind = i.zero? ? outlet : adapters[i-1]
-
-    skippable << adapter if (lookahead - lookbehind) <= 3
-  end
-
-  puts "Skippables: #{skippable.size}"
-  combinations = 0
-  (skippable.size + 1).times do |n|
-    combinations += skippable.combination(n).reject do |combination|
-      combination.each_cons(3).find { |x,y,z| y - x == 1 && z - y == 1}&.any?
-    end.size
-
-    puts n
-  end
-
-  combinations
-end
-
-adapters = File.read(File.expand_path('input.txt', File.dirname(__FILE__))).split("\n").map(&:to_i).sort
-puts get_joltage_differences(adapters)
-puts get_distinct_arrangements(adapters)
+puts AdapterSort.new(adapters).count_arrangements
